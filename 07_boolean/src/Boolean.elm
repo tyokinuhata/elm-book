ec2f978fcd251abd7ff434c6601ecb0f5f964496
Kazuki Tobita <kazukiti201@gmail.com>
2019-05-04 04:20:36 +0900

    Modify boolean

--- 07_boolean/src/Boolean.elm
+++ 07_boolean/src/Boolean.elm
@@ -1,3 +1,12 @@
 -- 真偽値はTrue/False
 bool1 = True
 bool2 = False
+
+-- 否定はnot演算子
+bool3 = not True
+bool4 = not False
+
+-- 論理積は&&演算子, 論理和は||演算子
+-- &&演算子は||演算子よりも優先される
+bool5 = True && False
+bool6 = True || False
