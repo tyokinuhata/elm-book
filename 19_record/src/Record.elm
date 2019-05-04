ad6686d6492b0a0c91cee6d67e92d6fef9389592
Kazuki Tobita <kazukiti201@gmail.com>
2019-05-04 17:05:16 +0900

    Add record

---
+++ 19_record/src/Record.elm
@@ -0,0 +1,14 @@
+-- レコード ... 名前付きのフィールドを持つデータ構造
+user = { id = 1, name = "Alice" }
+record1 = user.id
+record2 = user.name
+
+-- レコードの一部を更新して新しいレコードを作る
+-- 最初のuserのデータ自体は変更されていない
+-- Elmは全てのデータがイミュータブル(不変, Immutable)
+-- 一度作った値の中身を後から変更することができない
+newUser = { user | name = "ALICE" }
+
+-- レコードは .フィールド名 という関数でアクセスすることもできる
+record3 = .id user
+record4 = .name user
